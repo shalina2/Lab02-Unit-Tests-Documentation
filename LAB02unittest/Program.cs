927b4b9ca6202e29408354c70aad8b2cc847e363
shalom <shalom.belaineh@gmail.com>
2019-01-09 17:51:33 -0800

    set up the unit test and reference

---
+++ LAB02unittest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB02unittest
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
