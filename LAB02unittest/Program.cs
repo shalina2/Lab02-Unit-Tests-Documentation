426e1cbe3fa634a6e180455850b1e005b3ed34d6
shalom <shalom.belaineh@gmail.com>
2019-01-09 22:51:17 -0800

    working on balance and withdraw

--- LAB02unittest/Program.cs
+++ LAB02unittest/Program.cs
@@ -6,7 +6,46 @@ namespace LAB02unittest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("welcome to Shalom Bank");
+            Console.Write("what would you like to do: 1 viewbalance,2 withdraw or 3 Add money: ");
+            string userValue = Console.ReadLine();
+            int value = Convert.ToInt32(userValue);
+            viewbalance(value);
+
+            //string Valuechoose = Console.ReadLine();
+            //int number = Convert.ToInt32(Valuechoose);
+            withdraw(value);
         }
-    }
-}
+        static void viewbalance(int value)
+        {
+            if (value == 1)
+            {
+                Console.WriteLine("your account balace is $1000");
+                Console.ReadLine();
+            }
+        }
+           static void withdraw(int value)
+            {
+                if (value == 2)
+                {
+                    Console.WriteLine("how much would you like to withdraw");
+                   Console.Write("$20,$40,$80,$100: ");
+                Console.ReadLine();
+                Console.WriteLine("please take your cash from the ATM,anything else");
+                   Console.ReadLine();
+
+
+            }
+            }
+        }
+        
+    } 
+    
+       
+        
+            
+          
+        
+       
+    
+
